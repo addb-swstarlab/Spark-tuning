1fed38fe3fdbc6dfdd01a13e3a46d537f73bde55
dkdl012 <dkdl031@naver.com>
2023-10-12 14:18:34 +0900

    add configs on main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import argparse
 
 import baxus
 from baxus.util.behaviors import BaxusBehavior
@@ -9,62 +10,66 @@ from baxus.baxus import BAxUS
 from baxus.util.parsing import embedding_type_mapper, acquisition_function_mapper, mle_optimization_mapper
 
 from envs.spark import SparkParameters
+from models.utils import get_logger
 from models.bench import SparkBench
+from models.configs import baxus_params as bp
 
-embedding_type = "baxus" # "hesbo"
-acquisition_function = "ts" # "ei"
-mle_optimization = "sample-and-choose-best" # "multistart-gd"
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-t', '--test', type=str, default='test', help='description for this parameter')
+# parser.add_argument('-i', '--ints', type=int, default=5, help='description for this parameter')
+# parser.add_argument('-f', '--floats', type=float, default=0.5, help='description for this parameter')
+logger = get_logger()
 
-bin_sizing_method = embedding_type_mapper[embedding_type]
-acquisition_function = acquisition_function_mapper[acquisition_function]
-mle_optimization_method = mle_optimization_mapper[mle_optimization]
+# opt = parser.parse_args()
+# logger.info("## ARGUMENT INFORMATION ##")
+# for _ in vars(opt):
+#     logger.info(f"{_}: {vars(opt)[_]}")
 
-target_dim = 10 # args.target_dim
-n_init = 10 # args.n_init # initial samples (pre-observed samples)
-max_evals = 100 # args.max_evals
-noise_std = 0 # args.noise_std
-new_bins_on_split = None # args.new_bins_on_split
-multistart_samples = 100 # args.multistart_samples
-mle_training_steps = 50 # args.mle_training_steps
-multistart_after_samples = 10 # args.multistart_after_sample
-l_init = 0.8 # args.initial_baselength
-l_min = 0.5**7 # args.min_baselength
-l_max = 1.6 # args.max_baselength
-adjust_initial_target_dim = True # args.adjust_initial_target_dimension
-budget_until_input_dim = 0 # args.budget_until_input_dim
+def main():
+    bin_sizing_method = embedding_type_mapper[bp['embedding_type']]
+    acquisition_function = acquisition_function_mapper[bp['acquisition_function']]
+    mle_optimization_method = mle_optimization_mapper[bp['mle_optimization']]
 
-behavior = BaxusBehavior(n_new_bins=2,#new_bins_on_split,
-                         initial_base_length=l_init,
-                         min_base_length=l_min,
-                         max_base_length=l_max,
-                         acquisition_function=acquisition_function,
-                         embedding_type=bin_sizing_method,
-                         adjust_initial_target_dim=adjust_initial_target_dim,
-                         noise=noise_std,
-                         budget_until_input_dim=budget_until_input_dim
-                        )
+    behavior = BaxusBehavior(n_new_bins=2,#new_bins_on_split,
+                            initial_base_length=bp['l_init'],
+                            min_base_length=bp['l_min'],
+                            max_base_length=bp['l_max'],
+                            acquisition_function=acquisition_function,
+                            embedding_type=bin_sizing_method,
+                            adjust_initial_target_dim=bp['adjust_initial_target_dim'],
+                            noise=bp['noise_std'],
+                            budget_until_input_dim=bp['budget_until_input_dim']
+                            )
 
-gp_behaviour = GPBehaviour(
-    mll_estimation=mle_optimization_method,
-    n_initial_samples=multistart_samples,
-    n_best_on_lhs_selection=multistart_after_samples,
-    n_mle_training_steps=mle_training_steps,
-)
+    gp_behaviour = GPBehaviour(
+        mll_estimation=mle_optimization_method,
+        n_initial_samples=bp['multistart_samples'],
+        n_best_on_lhs_selection=bp['multistart_after_samples'],
+        n_mle_training_steps=bp['mle_training_steps'],
+    )
 
-sp = SparkParameters()
+    sp = SparkParameters()
 
-input_dim = len(sp) # args.input_dim
-ub = sp.ub
-lb = sp.lb
+    input_dim = len(sp) # args.input_dim
+    ub = sp.ub
+    lb = sp.lb
 
-f = SparkBench(dim=input_dim, ub=ub, lb=lb, sp=sp)
+    f = SparkBench(dim=input_dim, ub=ub, lb=lb, sp=sp)
 
-optim = BAxUS(f=f,
-              n_init=n_init,
-              max_evals=max_evals,
-              target_dim=target_dim,
-              behavior=behavior,
-              gp_behaviour=gp_behaviour
-              )
+    optim = BAxUS(f=f,
+                n_init=bp['n_init'],
+                max_evals=bp['max_evals'],
+                target_dim=bp['target_dim'],
+                behavior=behavior,
+                gp_behaviour=gp_behaviour
+                )
 
-optim.optimize()
+    optim.optimize()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        logger.exception("ERROR!!")
+    else:
+        logger.handlers.clear()
